c2552a04562bb32296f0dc2df692684e24626c9d
noqcks <benji@093b.org>
2024-04-04 17:18:19 +0000

    Upgrade dependencies

--- src/components/menus/TabNav/examples/TabNavReactRouterExample.tsx
+++ src/components/menus/TabNav/examples/TabNavReactRouterExample.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { NavLink, Switch, Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import { NavLink, Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
 import TabNav from '../TabNav';
 
 class ExampleBase extends React.Component<RouteComponentProps> {
